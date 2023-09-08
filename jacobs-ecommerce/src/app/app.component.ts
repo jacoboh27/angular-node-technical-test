8cc554db82de5ceeb24ae827f1b815e9f530901c
Jacobo <jacobohernandezisa@gmail.com>
2023-09-07 23:18:06 -0500

    adding html templates for customer ecommerce

--- jacobs-ecommerce/src/app/app.component.ts
+++ jacobs-ecommerce/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
+declare function HOMEINITTEMPLATE([]):any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jacobs-ecommerce';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      HOMEINITTEMPLATE($);
+    }, 50);
+  }
 }
